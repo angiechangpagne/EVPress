cc9ec390f06fb48a9807eb7d90ef24309c46827e
Angie <angiechangpagne@gmail.com>
2020-05-16 19:33:47 -0700

    fragments

---
+++ argon/README.md
@@ -0,0 +1,6 @@
+Argon Design Sys:
+
+
+1. npm install -g expo-cli
+2. npm install
+3.  expo start
---
+++ templateFragments.js
@@ -0,0 +1,1 @@
+fetch(`https://api.instagram.com/v1/media/search?lat=${lat}&lng=${lng}&distance=${distance}&client_id=${INSTAGRAM_CLIENT_ID}`)
